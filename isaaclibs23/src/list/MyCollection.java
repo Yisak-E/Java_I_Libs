11c334360d9df3830403144b1cec4a642a97cf8d
Yisak-E <yisakmetaferiya1@gmail.com>
2025-10-15 20:14:31 +0400

    reformating from scratch

---
+++ isaaclibs23/src/list/MyCollection.java
@@ -0,0 +1,13 @@
+interface MyCollection<K> {
+    int size();
+    boolean isEmpty();
+    boolean add(K k);
+    boolean remove(K k);
+    boolean contains(K k);
+    void clear();
+    Object[] toArray();
+    boolean containsAll(MyCollection<K> c);
+    boolean addAll(MyCollection< extends K> c);
+    boolean removeAll(MyCollection<K> c);
+    boolean retainAll(MyCollection<K> c);
+}
